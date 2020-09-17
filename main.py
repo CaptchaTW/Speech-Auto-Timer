81b3bf1ef9790034ae089bc7c782d05a0772092b
Yu-Chen Chu <yuchenmichaelchu@gmail.com>
2020-09-17 16:15:55 -0400

    initial basic structure

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import speech_recognition as sr
-import pyperclip
-
+from pynput import keyboard
+import pyautogui
 r = sr.Recognizer()
 mic = sr.Microphone()
+text = "hello"
+record_key = keyboard.Key.home
+typing_key = keyboard.Key.enter
 
-with mic as source:
-    audio = r.listen(source,phrase_time_limit = 4)
 
-    text = r.recognize_google(audio, language= 'en-CA')
+def on_press(key):
+    global text
+    try:
+        if key == record_key:
+            with mic as source:
+                audio = r.listen(source, phrase_time_limit=3)
+            try:
+                text = r.recognize_google(audio, language='en-CA')
+            except sr.UnknownValueError:
+                return True
+        elif key == typing_key:
+            pyautogui.typewrite(text)
+            return False
+    except AttributeError:
+        return True
 
-pyperclip.copy(text)
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
