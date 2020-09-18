3333d97168497f2654a092159b4d879f123e4227
Yu-Chen Chu <yuchenmichaelchu@gmail.com>
2020-09-17 22:16:46 -0400

    improved speech recognition bias

--- README.md
+++ README.md
@@ -5,5 +5,10 @@ League of Legends Speech recognition script to keep tracks of objective timers
 Use microphone to say when summonner spell got used. Sript then makes it easy to paste it into the chat with when the summonner spell will be back up.
 
 Keypresses:
-- Home: 3 seconds to record timer (summonner name + minutes and seconds it was used) ie: flash 2020 (twenty twenty, two zero two zero, 2 thousand 20)
+- Home: 3 seconds to record timer (role (top,jungle,middle,botton,middle) + summonner spell + minutes and seconds it was used) ie:  top flash 2020 (twenty twenty, two zero two zero, 2 thousand 20)
 - Enter: Types in all current timers that are still active  (in progress)
+
+Todo: 
+- Automatically remove timers that finishes
+- Find methodology to keep track of teleport(based on timer different cooldowns)
+- Improve speech recognition
--- main.py
+++ main.py
@@ -3,24 +3,29 @@ from pynput import keyboard
 import pyautogui
 import time
 import re
+import json
 r = sr.Recognizer()
 mic = sr.Microphone()
 text = ""
+json_file = None
 record_key = keyboard.Key.home
 typing_key = keyboard.Key.enter
-summoners = {"f": 300, "flash": 300, "cleanse": 210, "exhaust": 210, "ghost": 210, "heal": 240, "h": 240, "ignite": 180,
+summoners = {"flash": 300, "cleanse": 210, "exhaust": 210, "ghost": 210, "heal": 240, "ignite": 180,
              "barrier": 180}
+keywords = ["top", "jungle", "mid", "middle", "bot", "bottom", "support", "flash", "cleanse", "exhaust", "ghost",
+            "heal", "ignite", "barrier"]
 list_timers = []
 
 
 class Timers:
-    def __init__(self, game_component, start):
+    def __init__(self, game_component, start, role):
         self.game_component = game_component
         self.start = start
         self.end = summoners[game_component] + self.start
+        self.role = role
 
     def get_end(self):
-        return time.strftime("%M:%S", time.gmtime(self.end))
+        return self.role + " " + self.game_component + " " + time.strftime("%M:%S", time.gmtime(self.end))
 
 
 def on_press(key):
@@ -29,20 +34,34 @@ def on_press(key):
     try:
         if key == record_key:
             with mic as source:
-                audio = r.listen(source, phrase_time_limit=3)
+                audio = r.listen(source, phrase_time_limit=4)
             try:
-                text= r.recognize_google(audio, language='en-CA')
+                json_file = r.recognize_google(audio, language='en-CA', show_all=True)
+                most_likely = ""
+                highest_count = 0
+                for members in json_file["alternative"]:
+                    for transcript in members:
+                        current_count = 0
+                        for keys in keywords:
+                            if keys in str(members[transcript]).lower():
+                                current_count += 1
+                        if current_count > highest_count:
+                            most_likely = members[transcript]
+                            highest_count = current_count
+                text = most_likely.replace("/", "")
                 text = re.sub(r"([0-9]+)", r" \1 ", text.lower()).strip()
                 start_time = re.match('.*?([0-9]+)$', text).group(1)
-                summoner = re.match('^([\w]+)', text).group(1)
-                print(summoner, start_time)
-                timer_object = Timers(summoner, int(start_time)//100*60 + int(start_time)%100)
+                role = re.match('^([\w]+) (\w+)', text).group(1)
+                summoner = re.match('([\w]+?) (\w+)', text).group(2)
+                print(role, summoner, start_time)
+                timer_object = Timers(summoner, int(start_time)//100*60 + int(start_time)%100, role)
                 list_timers.append(timer_object)
             except sr.UnknownValueError:
+                print(key)
                 return True
         elif key == typing_key:
             for o in list_timers:
-                text_holder = text_holder + "" + str(o.get_end())
+                text_holder = text_holder + " " + str(o.get_end())
             pyautogui.typewrite(text_holder)
             return False
     except AttributeError:
